9e7ea9540e2e9099f45382c16d8901641c6d0752
asnganje <abd.nganje@gmail.com>
2024-02-03 20:38:54 +0300

    Wire up sign up form

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -2,39 +2,86 @@ import Footer from "../Footer";
 import Header from "../Header";
 import { Link } from "react-router-dom";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createUser } from "../../redux/thunks/userThunk";
+
 
 const SignUp = () => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const dispatch = useDispatch()
+
+    const fNameHandler = (e) => {
+        setFirstName(e.target.value)
+    }
+
+    const lNameHandler = (e) => {
+        setLastName(e.target.value)
+    }
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const user = {firstName, lastName, email, password}
+
+    const userCreateHandler = (e) => {
+        e.preventDefault()
+        dispatch(createUser(user))
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+        setPassword('')
+    }
+
     return(
         <div className="relative mx-[5%] h-screen">
             <Header />
             {<p className="text-green-500 text-2xl mx-[40%] font-mono">Success...</p>}
             <div className="flex relative flex-col gap-3 items-center justify-center text-xl font-mono mt-10">
                     <div className="bg-pink-200 p-5 shadow-lg rounded-md w-[100%] md:w-[70vh]">
-                        <form>
+                        <form onSubmit={userCreateHandler}>
                             <div className="mb-3 flex gap-5">
                                 <label className="mt-1">Firstname</label>
                                 <input
+                                value={firstName}
+                                onChange={fNameHandler}
                                 type="text"
                                 className="w-full rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
                                 />
                             </div>
                             <div className="mb-3 flex gap-5">
                                 <label className="mt-1">Lastname</label>
-                                <input 
+                                <input
+                                value={lastName}
+                                onChange={lNameHandler}
                                 type="text" 
                                 className="w-full ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300"
                                 />
                             </div>
                             <div className="mb-3 flex gap-5">
                                 <label className="mt-1">Email</label>
-                                <input 
+                                <input
+                                value={email}
+                                onChange={emailHandler}
                                 type="text"
                                 className="w-full ml-11 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
                                 />
                             </div>
                             <div className="mb-3 flex gap-5">
                                 <label className="mt-1">Password</label>
-                                <input 
+                                <input
+                                value={password}
+                                onChange={passwordHandler}
                                 type="text"
                                 className="w-full ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
                                 />
