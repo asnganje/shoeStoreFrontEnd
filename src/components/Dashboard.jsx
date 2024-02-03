305be7ce233d064d3fdc4783317079527be40696
asnganje <abd.nganje@gmail.com>
2024-02-03 19:06:34 +0300

    Add home section to dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,19 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Home from "./Home";
 
 const Dashboard = () => {
     return(
         <main className="h-screen mx-[5%]">
-            <Header />
-            Dashboard
-            
-            <Footer />
+            <section>
+                <Header />
+            </section>
+            <section>
+                <Home />
+            </section>
+            <section className="absolute bottom-0 left-0 w-[100%]">
+                <Footer />
+            </section> 
         </main>
     )
 }
