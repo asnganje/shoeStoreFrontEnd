13bd776884d23165b42a08b2a207033aa5d567f0
asnganje <abd.nganje@gmail.com>
2024-02-03 20:38:41 +0300

    add extra reducer(createUser) to slice

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createUser } from "../thunks/userThunk";
 
 const persons = {
     isLoading: false,
@@ -9,7 +10,16 @@ const userSlice = createSlice({
     name: 'user',
     initialState: persons,
     extraReducers(builder) {
-        builder.addCase()
+        builder.addCase(createUser.pending, (state) => {
+            state.isLoading = true;
+        }),
+        builder.addCase(createUser.fulfilled, (state, action)=> {
+            state.isLoading = false;
+            state.data.push(action.payload)
+        }),
+        builder.addCase(createUser.rejected, (state)=> {
+            state.isLoading = false;
+        })
     }
 })
 
