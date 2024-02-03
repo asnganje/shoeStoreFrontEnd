1504674f3aade512f0031a1ddfd13ec1c47e0b6c
asnganje <abd.nganje@gmail.com>
2024-02-03 16:21:39 +0300

    Test tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return(
-    <div>
+    <div className = "italic">
       Main App
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import './index.css'
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 const el = document.getElementById('root')
 const root = createRoot(el)
