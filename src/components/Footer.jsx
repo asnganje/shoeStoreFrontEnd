13728786f51171f864f64102088a88b45542bcfe
asnganje <abd.nganje@gmail.com>
2024-02-03 17:10:21 +0300

    Added footer logic

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import { nanoid } from "nanoid";
+import { TiSocialInstagram } from "react-icons/ti";
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { CiLinkedin } from "react-icons/ci";
+import { FaTelegramPlane } from "react-icons/fa";
+
+const data = [
+    {id: nanoid(), icon: <TiSocialInstagram/>},
+    {id: nanoid(), icon: <FaFacebook/>},
+    {id: nanoid(), icon: <FaXTwitter/>},
+    {id: nanoid(), icon: <CiLinkedin/>},
+    {id: nanoid(), icon: <FaTelegramPlane/>}
+]
+
+const date = new Date().getFullYear()
+const Footer = () => {
+    const renderedIcons = data.map((icon)=> {
+        return(
+            <li key={icon.id}>{icon.icon}</li>
+        )
+    })
+    return(
+        <div className="absolute bottom-0 shadow-lg text-blue-800 border left-0 flex flex-col justify-between w-[100%] gap-1 font-mono items-center">
+            <p>&copy; {date} TurkishFatma. All rights reserved.</p>
+            <ul className="font-mono mb-3 flex gap-2">{renderedIcons}</ul>
+        </div>
+    )
+}
+
+export default Footer;
