cb78693adfedbc408ebf8cca9f0998909e3f857d
asnganje <abd.nganje@gmail.com>
2024-02-03 21:32:39 +0300

    fix url in the thunk

--- src/redux/thunks/userThunk.js
+++ src/redux/thunks/userThunk.js
@@ -1,10 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000/api/v1/vtc"
+const baseUrl = "http://localhost:3000/api/v1/shoestore"
 const createUser = createAsyncThunk('user/createUser', async (user)=> {
+    const url = `${baseUrl}/signup`
     try {
-        await axios.post(baseUrl, user)
+        await axios.post(url, user)
     } catch (error) {
         throw new Error(`User could not be created due to ${error}`)
     }
