d44b1ab378026d317a4326e565cb01918481db61
asnganje <abd.nganje@gmail.com>
2024-02-03 17:01:42 +0300

    add header logic

---
+++ src/components/Header.jsx
@@ -0,0 +1,154 @@
+import { Link } from "react-router-dom";
+import { nanoid } from "nanoid";
+import shoeLogo from "../assets/shoes.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
+import { useState, useEffect } from "react";
+
+const data = [
+  { id: nanoid(), text: "Products", route: "/vtcs" },
+  { id: nanoid(), text: "Stores", route: "/courses" },
+];
+const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const logOutHandler = () => {};
+
+  const toggleNav = () => {
+    setIsNavOpen(true);
+  };
+
+  const handleReToggle = () => {
+    setIsNavOpen(false);
+  };
+
+  const handleHeaderItemClick = () => {};
+
+  const renderedHeaderItems = data.map((item) => {
+    return (
+      <li
+        key={item.id}
+        onClick={() => handleHeaderItemClick(item.id)}
+        className="text-blue-400 cursor-pointer p-1 text-xl hover:bg-blue-500 hover:rounded-md hover:text-white"
+      >
+        {item.text}
+      </li>
+    );
+  });
+  return (
+    <>
+      <div className="flex items-center justify-between shadow-lg">
+        <div className="w-[10%] h-[10%] cursor-pointer">
+          <Link to="/">
+            <img
+              src={shoeLogo}
+              alt="Turkish shoe logo"
+              className="rounded-md m-[5%]"
+            />
+          </Link>
+        </div>
+        {!isSmallScreen ? (
+          <div className="flex items-center space-x-4">
+            <nav>
+              <ul className="flex space-x-4">{renderedHeaderItems}</ul>
+            </nav>
+            {
+              <div className="p-2 flex items-center space-x-4 w-full">
+                {
+                  <div className="text-blue-800 p-2 text-xl">
+                    <Link to="/login">
+                      <button className="p-1 rounded-md hover:bg-blue-400 hover:text-white">
+                        Login
+                      </button>
+                    </Link>{" "}
+                    <span className="text-orange-500">|</span>{" "}
+                    <Link to="/register">
+                      <button className="p-1 rounded-md hover:bg-blue-400 hover:text-white">
+                        Register
+                      </button>
+                    </Link>
+                  </div>
+                }
+                {
+                  <p className="flex sm:flex-row gap-2 flex-col">
+                    <span className="mt-[1vh] cursor-pointer">Welcome</span>
+                    <Link to="/">
+                      <button
+                        className="text-white bg-blue-500 p-1 rounded-md text-xl hover:bg-blue-400"
+                        onClick={logOutHandler}
+                      >
+                        Logout
+                      </button>
+                    </Link>
+                  </p>
+                }
+              </div>
+            }
+          </div>
+        ) : (
+          <div className="cursor-pointer text-4xl p-1 text-blue-500 md:hidden">
+            {!isNavOpen ? (
+              <RxHamburgerMenu onClick={toggleNav} />
+            ) : (
+              <RxCross2 onClick={handleReToggle} />
+            )}
+          </div>
+        )}
+      </div>
+      {isSmallScreen && isNavOpen && (
+        <div className="flex shadow-lg mb-5 flex-col h-[50vh]">
+          <ul className="flex flex-col w-[50%] mt-[2%]">
+            {renderedHeaderItems}
+          </ul>
+          {
+            <div className="flex w-full">
+              {
+                <div className="text-blue-800 m-0 p-2 text-xl">
+                  <Link to="/login">
+                    <button className="p-1 rounded-md hover:bg-blue-400 hover:text-white">
+                      Login
+                    </button>
+                  </Link>{" "}
+                  <span className="text-orange-500">|</span>{" "}
+                  <Link to="/register">
+                    <button className="p-1 rounded-md hover:bg-blue-400 hover:text-white">
+                      Register
+                    </button>
+                  </Link>
+                </div>
+              }
+              {
+                <p className="flex sm:flex-row gap-2 flex-col">
+                  <span className="mt-[1vh] cursor-pointer">Welcome</span>
+                  <Link to="/">
+                    <button
+                      className="text-white bg-blue-500 p-1 rounded-md text-xl hover:bg-blue-400"
+                      onClick={logOutHandler}
+                    >
+                      Logout
+                    </button>
+                  </Link>
+                </p>
+              }
+            </div>
+          }
+        </div>
+      )}
+    </>
+  );
+};
+export default Header;
