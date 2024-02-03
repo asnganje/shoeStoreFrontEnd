87cf84ee4f29d908784c3c3deb27671e36e6875b
asnganje <abd.nganje@gmail.com>
2024-02-03 19:42:28 +0300

    import relevant dependencies for signup

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,6 +1,59 @@
+import Footer from "../Footer";
+import Header from "../Header";
+import { Link } from "react-router-dom";
+import { IoMdArrowRoundBack } from "react-icons/io";
+
 const SignUp = () => {
     return(
-        <div>SignUp</div>
+        <div className="relative mx-[5%] h-screen">
+            <Header />
+            {<p className="text-green-500 text-2xl mx-[40%] font-mono">Success...</p>}
+            <div className="flex relative flex-col gap-3 items-center justify-center text-xl font-mono mt-10">
+                    <div className="bg-pink-200 p-5 shadow-lg rounded-md w-[100%] md:w-[70vh]">
+                        <form>
+                            <div className="mb-3 flex gap-5">
+                                <label className="mt-1">Firstname</label>
+                                <input
+                                type="text"
+                                className="w-full rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
+                                />
+                            </div>
+                            <div className="mb-3 flex gap-5">
+                                <label className="mt-1">Lastname</label>
+                                <input 
+                                type="text" 
+                                className="w-full ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300"
+                                />
+                            </div>
+                            <div className="mb-3 flex gap-5">
+                                <label className="mt-1">Email</label>
+                                <input 
+                                type="text"
+                                className="w-full ml-11 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
+                                />
+                            </div>
+                            <div className="mb-3 flex gap-5">
+                                <label className="mt-1">Password</label>
+                                <input 
+                                type="text"
+                                className="w-full ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
+                                />
+                            </div>
+                            <button className="text-white rounded-md w-[50%] mx-[35%] bg-gray-400 hover:bg-gray-500 p-2">Register</button>
+                        </form>
+                        <div className="flex gap-5 mt-5">
+                            <p className="text-gray-600 italic space-x-0">Already registered?</p><Link to='/login' className="text-gray-600 hover:text-blue-500 hover:underline">Login here!</Link>
+                        </div>
+                    </div>
+                    
+                    <Link to="/" className="h-[10%]">
+                        <p className=" flex items-center cursor-pointer hover:underline hover:text-blue-400 mb-[5vh]">
+                            <IoMdArrowRoundBack />Back to home
+                        </p>
+                    </Link>
+                </div>
+                <Footer/>
+            </div>
     )
 }
 
