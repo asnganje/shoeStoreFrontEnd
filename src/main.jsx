f757a6eac4ed9d11bee06d727048e8d4642e6ca4
asnganje <abd.nganje@gmail.com>
2024-02-03 20:31:55 +0300

    connect react to redux store

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import './index.css'
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import {store} from './redux/store/index.js'
 
 const el = document.getElementById('root')
 const root = createRoot(el)
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
