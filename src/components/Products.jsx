ad8c77b4deaabaf1f18a9354e91508ceca145c07
asnganje <abd.nganje@gmail.com>
2024-02-04 19:00:00 +0300

    Add products component logic

---
+++ src/components/Products.jsx
@@ -0,0 +1,39 @@
+import Footer from "./Footer"
+import Header from "./Header"
+
+const Product = () => {
+    return(
+        <div className="relative h-screen mx-[5%]">
+            <Header />
+            <div className=" mx-[35%] mt-[10%] bg-pink-200 p-5 shadow-lg rounded-md w-[100%] md:w-[70vh]">
+                        <form>
+                            <div className="mb-3 flex gap-5">
+                                <label className="mt-1">Name</label>
+                                <input
+                                type="text"
+                                className="w-full rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
+                                />
+                            </div>
+                            <div className="mb-3 flex gap-5">
+                                <label className="mt-1">Price</label>
+                                <input
+                                type="text" 
+                                className="w-full ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300"
+                                />
+                            </div>
+                            <div className="mb-3 flex gap-5">
+                                <label className="mt-1">Store</label>
+                                <input
+                                type="text"
+                                className="w-full ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
+                                />
+                            </div>
+                            <button className="text-white rounded-md w-[50%] mx-[35%] bg-gray-400 hover:bg-gray-500 p-2">Add Product</button>
+                        </form>
+                    </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Product;
