da7734ad0b35acb60eaec808f60eda3338279c15
asnganje <abd.nganje@gmail.com>
2024-02-04 17:00:10 +0300

    Modify store styling

---
+++ src/components/Store.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer"
+import Header from "./Header"
+
+const Store = () => {
+    return(
+        <div className="relative h-screen mx-[5%]">
+            <Header />
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Store;
