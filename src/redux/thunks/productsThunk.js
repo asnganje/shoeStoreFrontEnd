cc566b026202f0409b17d31fc96721de6dde4ec9
asnganje <abd.nganje@gmail.com>
2024-02-05 08:01:13 +0300

    Modify store route

--- src/redux/thunks/productsThunk.js
+++ src/redux/thunks/productsThunk.js
@@ -2,14 +2,26 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const baseUrl = "http://localhost:3000/api/v1/shoestore"
-const getAllProducts = createAsyncThunk('products/getAllProducts', async (storeId)=> {
+
+const createProduct = createAsyncThunk('products/createProduct', async(product)=> {
     const url = `${baseUrl}/products`
     try {
-        const response = await axios.get(url, storeId)
+        const response = await axios.post(url, product)
+        return response.data.msg
+    } catch (error) {
+        throw new Error(`User could not be created due to ${error}`) 
+    }
+})
+
+
+const getAllProducts = createAsyncThunk('products/getAllProducts', async (store)=> {
+    const url = `${baseUrl}/products?store=${store.store}`
+    try {
+        const response = await axios.get(url)
         return response.data.msg
     } catch (error) {
         throw new Error(`User could not be created due to ${error}`)
     }
 })
 
-export {getAllProducts}
+export {getAllProducts, createProduct}
