7113abfb8d865a7b42fd2936310e9bd6607814a0
asnganje <abd.nganje@gmail.com>
2024-02-05 08:00:49 +0300

    Add get stores logic

--- src/redux/slices/storeSlice.js
+++ src/redux/slices/storeSlice.js
@@ -1,26 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getStores } from "../thunks/storeThunk";
+import { createStore, getStores } from "../thunks/storeThunk";
 
 const naaStore = {
-    isLoading: false,
-    data: []
-}
+  isLoading: false,
+  data: [],
+};
 
 const storeSlice = createSlice({
-    name: 'store',
-    initialState: naaStore,
-    extraReducers(builder) {
-        builder.addCase(getStores.pending, (state)=> {
-            state.isLoading = true;
-        }),
-        builder.addCase(getStores.fulfilled, (state, action)=> {
-            state.isLoading = true;
-            state.data = [...action.payload]
-        }),
-        builder.addCase(getStores.rejected, (state)=> {
-            state.isLoading = true;
-        })
-    }
-})
+  name: "store",
+  initialState: naaStore,
+  extraReducers(builder) {
+    builder.addCase(getStores.pending, (state) => {
+      state.isLoading = true;
+    }),
+      builder.addCase(getStores.fulfilled, (state, action) => {
+        state.isLoading = true;
+        state.data = [...action.payload];
+      }),
+      builder.addCase(getStores.rejected, (state) => {
+        state.isLoading = true;
+      }),
+      builder.addCase(createStore.pending, (state)=> {
+        state.isLoading = true;
+      }),
+      builder.addCase(createStore.fulfilled, (state, action)=> {
+        state.isLoading = false;
+        state.data.push(action.payload)
+      }),
+      builder.addCase(createStore.rejected, (state)=> {
+        state.isLoading = false;
+      })
+  },
+});
 
 export const storeReducer = storeSlice.reducer;
