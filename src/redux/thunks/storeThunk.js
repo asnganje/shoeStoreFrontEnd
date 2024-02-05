be02d3059e30abae3985ad6c34bc5333fb2bef65
asnganje <abd.nganje@gmail.com>
2024-02-05 08:01:33 +0300

    Modify store thunk

--- src/redux/thunks/storeThunk.js
+++ src/redux/thunks/storeThunk.js
@@ -3,6 +3,16 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:3000/api/v1/shoestore"
 
+const createStore = createAsyncThunk('store/createStore', async(store)=> {
+    const url = `${baseUrl}/store`
+    try {
+        const response = await axios.post(url, store)
+        return response.data.msg
+    } catch (error) {
+        throw new Error(`User could not be created due to ${error}`)        
+    }
+})
+
 const getStores = createAsyncThunk('store/getStores', async()=> {
     const url = `${baseUrl}/store`
     try {
@@ -13,4 +23,4 @@ const getStores = createAsyncThunk('store/getStores', async()=> {
     }
 }) 
 
-export {getStores}
+export {getStores, createStore}
