fffc0bdf731f3f22bbcc38118409033f5d3d4b18
asnganje <abd.nganje@gmail.com>
2024-02-05 07:59:47 +0300

    Modify storecreate component

--- src/components/StoreCreate.jsx
+++ src/components/StoreCreate.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react"
 import Footer from "./Footer"
 import Header from "./Header"
+import { useDispatch } from "react-redux"
+import { createStore, getStores } from "../redux/thunks/storeThunk"
+import { useNavigate } from "react-router-dom"
 
 const StoreCreate = () => {
+
+    const [name, setName] = useState('')
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleNameChange = (e)=> {
+        setName(e.target.value)
+    }
+
+    const newStore = {name}
+
+    const handleStoreNameSubmit = (e)=> {
+        e.preventDefault();
+        dispatch(createStore(newStore)).then(()=> {
+            dispatch(getStores())
+        })
+        setName('')
+        navigate('/store')
+    }
+
     return(
         <div className="relative h-screen mx-[5%]">
             <Header />
             <div className=" mx-[35%] mt-[10%] bg-pink-200 p-5 shadow-lg rounded-md w-[100%] md:w-[70vh]">
-                        <form>
+                        <form onSubmit={handleStoreNameSubmit}>
                             <div className="mb-3 flex gap-5">
                                 <label className="mt-1">Name</label>
                                 <input
+                                value={name}
+                                onChange={handleNameChange}
                                 type="text"
                                 className="w-full rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
                                 />
